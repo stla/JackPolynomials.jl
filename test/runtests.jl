fe2ef4b725ad65a5a909d97b524f07fa4c3e6da3
stla <laurent_step@yahoo.fr>
2020-12-15 13:33:03 +0100

    tests Schur

--- test/runtests.jl
+++ test/runtests.jl
@@ -38,3 +38,23 @@ end
     @test isapprox(JackPolynomial(4, [4], 2.5)(2.0, 3.0, 4.0, 5.0), 500338.5)
   end
 end
+
+@testset "Schur" begin
+  @testset "Schur expansion of (sum x_i)^n" begin
+    x = [3.0; 4.0; 5.0; 6.0]
+    s = Schur(x, [4]) + 3*Schur(x, [3,1]) + 2*Schur(x, [2,2]) +
+      3*Schur(x, [2,1,1]) + Schur(x, [1,1,1,1])
+    @test isapprox(s, sum(x)^4)
+  end 
+  @testset "Schur = 0 when l(lambda)>l(x)" begin
+    @test isapprox(Schur([1.0; 2.0], [3; 2; 1]), 0.0)
+  end
+  @testset "Schur (3,2)" begin
+    x = [0.3, 2.0, 5.0]
+    expected = x[1]^3*x[2]^2 + x[1]^3*x[3]^2 + x[1]^3*x[2]*x[3] +
+      x[1]^2*x[2]^3 + x[1]^2*x[3]^3 + 2*x[1]^2*x[2]*x[3]^2 +
+      2*x[1]^2*x[2]^2*x[3] + x[1]*x[2]*x[3]^3 + 2*x[1]*x[2]^2*x[3]^2 +
+      x[1]*x[2]^3*x[3] + x[2]^2*x[3]^3 + x[2]^3*x[3]^2
+    @test isapprox(Schur(x, [3; 2]), expected)
+  end
+end
